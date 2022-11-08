61d9f07dca63f7ba8eed57d2e08f591712e80489
foysal1021 <foysala1021@gmail.com>
2022-11-08 17:44:21 +0600

    added mongo conect

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ const express = require("express");
 const app = express();
 const port = 5000 || process.env.PROT;
 var cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 
 //midle were start
 app.use(cors());
 app.use(express.json());
 //midle were end
+
+// mongo conect start
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uvaek7y.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// mongo conect end
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
